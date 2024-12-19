01fcd66772bea2c65817408fa7fbd69312505798
Lalit Rajak <lalitrajak312@gmail.com>
2024-12-19 15:28:51 +0530

    added glanceCard

--- src/components/GlanceCard.tsx
+++ src/components/GlanceCard.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import growIcon from "@/assets/grow.svg";
+import downIcon from "@/assets/down.svg";
+import Image from "next/image";
 
-const GlanceCard = () => {
-  return <div>GlanceCard</div>;
+interface GlanceCardProps {
+  data: {
+    icon: string;
+    title: string;
+    no: string;
+    percentage: number;
+    status: string;
+  };
+}
+
+const GlanceCard = ({ data }: GlanceCardProps) => {
+  const { icon, title, no, percentage, status } = data;
+  return (
+    <div className="card border shadow-lg flex flex-col justify-between">
+      <div className="flex items-center gap-1">
+        <Image src={icon} alt="icon" width={15} height={15} />
+        <h1 className="text-[12px] font-medium tracking-wider">{title}</h1>
+      </div>
+
+      <h1 className="text-[24px] font-medium">{no}</h1>
+      <div className="flex items-center gap-1">
+        {status === "increase" ? (
+          <Image src={growIcon} alt="icon" width={25} height={15} />
+        ) : (
+          <Image src={downIcon} alt="icon" width={25} height={15} />
+        )}
+
+        <h1 className="text-[14px] space-x-2 font-normal">
+          <span
+            className={`${
+              status === "increase" ? "text-[#15B79F]" : "text-[#F04438]"
+            }`}
+          >
+            {percentage}%
+          </span>
+          <span>{status}</span>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default GlanceCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,56 @@
+import GlanceCard from "@/components/GlanceCard";
 import React from "react";
+import label from "@/assets/smalltag.svg";
+import coin from "@/assets/Coin.svg";
+import coins from "@/assets/Coins.svg";
+import chat from "@/assets/Vector.svg";
+
+const GlanceCardData = [
+  {
+    icon: chat as string,
+    title: "CONSULTATIONS",
+    no: "24",
+    percentage: 15,
+    status: "increase",
+  },
+  {
+    icon: label as string,
+    title: "ORDERS PLACED",
+    no: "12",
+    percentage: 15,
+    status: "decrease",
+  },
+  {
+    icon: coins as string,
+    title: "CONSULTATIONS",
+    no: "$2,400",
+    percentage: 15,
+    status: "increase",
+  },
+  {
+    icon: coin as string,
+    title: "AVG ORDER VALUE",
+    no: "$240",
+    percentage: 15,
+    status: "increase",
+  },
+];
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="m-10">
+      <div className="space-y-10 ">
+        <div className="flex justify-between items-center">
+          <h1 className="text-[32px] font-normal">At a glance</h1>
+        </div>
+        <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-4">
+          {GlanceCardData.map((data, idx) => (
+            <GlanceCard key={idx} data={data} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
